f02b61114973d4b89a278cf2e0095870469329f0
anothermist <anothermist@gmail.com>
2022-04-13 21:20:17 +0500

    Button + Led

---
+++ BUTTON/main/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
+
+void app_main(void)
+{
+
+
+    while (1) {
+
+    }
+}
