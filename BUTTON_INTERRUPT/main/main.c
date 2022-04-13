22b2a718e6f332418e4a3cd01190f807c0a91f57
anothermist <anothermist@gmail.com>
2022-04-14 00:28:45 +0500

    Interrupt example

--- BUTTON_INTERRUPT/main/main.c
+++ BUTTON_INTERRUPT/main/main.c
@@ -1,22 +1,63 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
 #include "sdkconfig.h"
 
+#define GPIO_OUTPUT_PIN_SEL  (1ULL<<CONFIG_BLINK_GPIO)
+#define GPIO_INPUT_PIN_SEL  ((1ULL<<CONFIG_BUTTON_GPIO_1) | (1ULL<<CONFIG_BUTTON_GPIO_2))
+#define ESP_INTR_FLAG_DEFAULT 0
+
+static xQueueHandle gpio_evt_queue = NULL;
+
+static void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+	uint32_t gpio_num = (uint32_t) arg;
+	xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+static void task1(void* arg)
+{
+	uint32_t io_num;
+	for(;;) {
+		if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+			printf("GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
+		}
+	}
+}
+
 void app_main(void)
 {
-	int led_gpio = CONFIG_BLINK_GPIO;
-	int button_gpio = CONFIG_BUTTON_GPIO;
-	gpio_reset_pin(led_gpio);
-	gpio_reset_pin(button_gpio);
-	gpio_set_direction(led_gpio, GPIO_MODE_OUTPUT);
-	gpio_set_direction(button_gpio, GPIO_MODE_INPUT);
-	gpio_set_pull_mode(button_gpio, GPIO_PULLUP_ONLY);
+	int cnt = 0;
+	gpio_config_t io_conf = {};
+	io_conf.intr_type = GPIO_INTR_DISABLE;
+	io_conf.mode = GPIO_MODE_OUTPUT;
+	io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+	io_conf.pull_down_en = 0;
+	io_conf.pull_up_en = 0;
+	gpio_config(&io_conf);
+	io_conf.intr_type = GPIO_INTR_POSEDGE;
+	io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+	io_conf.mode = GPIO_MODE_INPUT;
+	io_conf.pull_up_en = 1;
+	gpio_config(&io_conf);
+
+	gpio_set_intr_type(CONFIG_BUTTON_GPIO_1, GPIO_INTR_ANYEDGE);
+
+	gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+
+	xTaskCreate(task1, "task1", 2048, NULL, 10, NULL);
+
+	gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+
+	gpio_isr_handler_add(CONFIG_BUTTON_GPIO_1, gpio_isr_handler, (void*) CONFIG_BUTTON_GPIO_1);
+	gpio_isr_handler_add(CONFIG_BUTTON_GPIO_2, gpio_isr_handler, (void*) CONFIG_BUTTON_GPIO_2);
+
 	while (1) {
-		if(gpio_get_level(button_gpio)) gpio_set_level(led_gpio, 0);
-		else gpio_set_level(led_gpio, 1);
-		vTaskDelay(10 / portTICK_PERIOD_MS);
+		printf("cnt: %d\n", cnt++);
+		gpio_set_level(CONFIG_BLINK_GPIO, cnt % 2);
+		vTaskDelay(1000 / portTICK_RATE_MS);
 	}
 }
